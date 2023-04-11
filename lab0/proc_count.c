64ec696e33366643bed10b4471d126622bf18fc1
Eric Ren <eric1423247764@gmail.com>
2023-04-11 13:36:54 -0700

    changed the name to proc_count and file name in

--- lab0/proc_count.c
+++ lab0/proc_count.c
@@ -15,17 +15,8 @@
 // 	pr_info("proc_count: exit\n");
 // }
 
-// module_init(proc_count_init);
-// module_exit(proc_count_exit);
 
-// MODULE_AUTHOR("Minhao Ren");
-// MODULE_DESCRIPTION("Count and return the current number of running processes");
-// MODULE_LICENSE("GPL");
 
-// proc_create_single 
-// proc_remove 
-// for_each_process
-// seq_printf
 
 //Define a pointer to a proc_dir_entry struct called entry. 
 //This will be used to create and remove the virtual file in the /proc filesystem.
@@ -33,7 +24,7 @@ static struct proc_dir_entry *entry;
 
 //Define the example function, which takes a pointer to a seq_file struct and a void pointer. 
 //This function is called when the virtual file is read. 
-static int example(struct seq_file *m, void *v){
+static int proc_count(struct seq_file *m, void *v){
 	//It first prints "hello world" to the seq_file, 
 	seq_printf(m, "hello world\n");
 	struct task_struct *p;
@@ -48,12 +39,12 @@ static int example(struct seq_file *m, void *v){
 }
 
 //Called when the module is loaded into the kernel. 
-static int __init example_init(void)
+static int __init proc_count_init(void)
 {
 	//This function creates the virtual file "example_virtual_file" 
 	//in the /proc filesystem with a proc_create_single call, 
 	//using the example function as a callback for file reads.
-	entry = proc_create_single("example_virtual_file", 0, NULL, example);
+	entry = proc_create_single("count", 0, NULL, example);
 	//Then, it prints "example: init" to the kernel log to indicate that the module has been initialized. 
 	//The function returns 0 to indicate success.
 	pr_info("example: init\n");
@@ -64,15 +55,15 @@ static int __init example_init(void)
 //This function removes the virtual file from the /proc filesystem 
 //using the proc_remove function and then prints "example: exit" to the kernel log 
 //to indicate that the module has been removed.
-static void __exit example_exit(void)
+static void __exit proc_count_exit(void)
 {
 	proc_remove(entry);
 	pr_info("example: exit\n");
 }
 
-module_init(example_init);
-module_exit(example_exit);
+module_init(proc_count_init);
+module_exit(proc_count_exit);
 
-MODULE_AUTHOR("Yadi Cao");
-MODULE_DESCRIPTION("CS111 lab0 random play around");
+MODULE_AUTHOR("Minhao Ren");
+MODULE_DESCRIPTION("Count and return the current number of running processes");
 MODULE_LICENSE("GPL");
