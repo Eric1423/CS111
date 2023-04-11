7de3d96c603891ebaf54db5df838657e8366b4de
Eric Ren <eric1423247764@gmail.com>
2023-04-11 13:17:47 -0700

    example file uploaded

--- lab0/proc_count.c
+++ lab0/proc_count.c
@@ -4,20 +4,75 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
-static int __init proc_count_init(void)
-{
-	pr_info("proc_count: init\n");
+// static int __init proc_count_init(void)
+// {
+// 	pr_info("proc_count: init\n");
+// 	return 0;
+// }
+
+// static void __exit proc_count_exit(void)
+// {
+// 	pr_info("proc_count: exit\n");
+// }
+
+// module_init(proc_count_init);
+// module_exit(proc_count_exit);
+
+// MODULE_AUTHOR("Minhao Ren");
+// MODULE_DESCRIPTION("Count and return the current number of running processes");
+// MODULE_LICENSE("GPL");
+
+// proc_create_single 
+// proc_remove 
+// for_each_process
+// seq_printf
+
+//Define a pointer to a proc_dir_entry struct called entry. 
+//This will be used to create and remove the virtual file in the /proc filesystem.
+static struct proc_dir_entry *entry;
+
+//Define the example function, which takes a pointer to a seq_file struct and a void pointer. 
+//This function is called when the virtual file is read. 
+static int example(struct seq_file *m, void *v){
+	//It first prints "hello world" to the seq_file, 
+	seq_printf(m, "hello world\n");
+	struct task_struct *p;
+	int sqr_sum = 0;
+	//then iterates over each process in the system using the for_each_process macro. 
+	for_each_process(p) {
+		//For each process, it prints "hello world" to the seq_file. 
+		seq_printf(m, "hello world\n");
+	}
+	//The function then returns 0 to indicate success.
 	return 0;
 }
 
-static void __exit proc_count_exit(void)
+//Called when the module is loaded into the kernel. 
+static int __init example_init(void)
 {
-	pr_info("proc_count: exit\n");
+	//This function creates the virtual file "example_virtual_file" 
+	//in the /proc filesystem with a proc_create_single call, 
+	//using the example function as a callback for file reads.
+	entry = proc_create_single("example_virtual_file", 0, NULL, example);
+	//Then, it prints "example: init" to the kernel log to indicate that the module has been initialized. 
+	//The function returns 0 to indicate success.
+	pr_info("example: init\n");
+	return 0;
 }
 
-module_init(proc_count_init);
-module_exit(proc_count_exit);
+//It is called when the module is unloaded from the kernel. 
+//This function removes the virtual file from the /proc filesystem 
+//using the proc_remove function and then prints "example: exit" to the kernel log 
+//to indicate that the module has been removed.
+static void __exit example_exit(void)
+{
+	proc_remove(entry);
+	pr_info("example: exit\n");
+}
 
-MODULE_AUTHOR("Your name");
-MODULE_DESCRIPTION("One sentence description");
+module_init(example_init);
+module_exit(example_exit);
+
+MODULE_AUTHOR("Yadi Cao");
+MODULE_DESCRIPTION("CS111 lab0 random play around");
 MODULE_LICENSE("GPL");
