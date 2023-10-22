c616dd5cd6f5ee559092b20b7ec17221d624dc71
Eric Ren <eric1423247764@gmail.com>
2023-10-22 15:34:20 -0700

    added lab1 and lab2——1

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -7,84 +7,115 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc <= 1) {
-		// Invalid argument (less than one)
+	// Check if no command arguments are provided
+	if (argc <= 1)
+	{
+		// Exit with an error status indicating invalid argument
 		exit(EINVAL);
 	}
-	if (argc == 2) {
-		// Execute directly if only one argument
+
+	// If only one command argument is provided
+	if (argc == 2)
+	{
+		// Create a child process
 		pid_t cpid = fork();
-		if (cpid < 0) {
+
+		// If fork fails
+		if (cpid < 0)
+		{
 			perror("Fork failed");
 			exit(EXIT_FAILURE);
 		}
-		if (cpid == 0) {
-			if (execlp(argv[1], argv[1], NULL) < 0) {
+
+		// If inside child process
+		if (cpid == 0)
+		{
+			// Execute the command provided as argument
+			if (execlp(argv[1], argv[1], NULL) < 0)
+			{
 				perror("Executing command failed");
-        		exit(EXIT_FAILURE);
+				exit(EXIT_FAILURE);
 			}
 		}
-		else {
+		// If inside parent process
+		else
+		{
 			int status = 0;
+			// Wait for child process to terminate and get its status
 			waitpid(cpid, &status, 0);
+			// Exit with the child's exit status
 			exit(WEXITSTATUS(status));
 		}
 	}
-	else {
-		// More than two arguments
-		int fds[2];
-		int prevEnd = -1;
+	// If more than one command argument is provided
+	else
+	{
+		int fds[2];		  // File descriptors for the pipe
+		int prevEnd = -1; // Previous pipe's read end
 
-		for (int i = 1; i < argc; i++) {
+		// Loop through all provided commands
+		for (int i = 1; i < argc; i++)
+		{
 
-			if (pipe(fds) < 0) {
+			// Create a pipe
+			if (pipe(fds) < 0)
+			{
 				perror("Pipe creation failed");
 				exit(EXIT_FAILURE);
 			}
 
+			// Create a child process
 			int pid = fork();
-			if (pid < 0) {
+
+			if (pid < 0)
+			{
 				perror("Fork failed");
 				exit(EXIT_FAILURE);
-			} 
-			else if (pid == 0) {
-				// Child process
-				if (prevEnd != -1) {
-					// Redirect stdin to the read end of previous pipe
+			}
+			// If inside child process
+			else if (pid == 0)
+			{
+				// If there was a previous command, set its output as the current command's input
+				if (prevEnd != -1)
+				{
 					dup2(prevEnd, STDIN_FILENO);
 					close(prevEnd);
 				}
 
-				// If not the last command
-				if (i < argc - 1) {
-					// Redirect stdout to write end of current pipe
+				// If not the last command, set the output to the write end of the pipe
+				if (i < argc - 1)
+				{
 					dup2(fds[1], STDOUT_FILENO);
 				}
 
+				// Close unused pipe ends
 				close(fds[0]);
 				close(fds[1]);
 
 				// Execute the current command
 				execlp(argv[i], argv[i], NULL);
 				exit(EXIT_FAILURE);
-			} 
-			else {
-				// Parent process
-				close(fds[1]);
-				prevEnd = fds[0];
+			}
+			// If inside parent process
+			else
+			{
+				close(fds[1]);	  // Close write end of the pipe
+				prevEnd = fds[0]; // Store read end for next iteration
 
-				// Wait for child process
 				int status = 0;
+				// Wait for child process to terminate and get its status
 				waitpid(pid, &status, 0);
 
-				if (!WIFEXITED(status)) {
-					// Child process terminate failed
+				// If child did not terminate normally, exit with failure
+				if (!WIFEXITED(status))
+				{
 					exit(EXIT_FAILURE);
 				}
 
+				// If child exited with a non-zero status, exit with the same status
 				int exitStatus = WEXITSTATUS(status);
-				if (exitStatus != 0) {
-					// Child process returned an error
+				if (exitStatus != 0)
+				{
 					exit(exitStatus);
 				}
 			}
